909e04cc8b71317eb2cf60cf1f5d8d51446d3bbf
ReZ0iro <rezomoon@gmail.com>
2023-06-17 21:07:33 +0430

    Customize UserAdmin in DjangoAdmin

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import UserAccounts 
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+class AdminUserAccounts(UserAdmin) : 
+    list_display = ("id" , "username",  "user_role", "password")
+    filter_horizontal = ("following",)
+    list_filter = ("following",)
+    fieldsets = ()
