cac1f7c3e22f8ba1dbab57b9ed0d10a23c673b5f
ReZ0iro <rezomoon@gmail.com>
2023-06-19 17:28:26 +0430

    Add UserComments And UserPost to posts.admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from posts.models import UserComments , UserPost
 
-# Register your models here.
+# Register your models here. 
+class AdminUserPosts(admin.ModelAdmin) : 
+
+    list_display = ("text" , "the_user")
+
+admin.site.register(UserPost,AdminUserPosts)
+
+class AdminUserComments(admin.ModelAdmin) : 
+
+    list_display = ("comment_text" , "user_comment" , "user_post")
+    
+admin.site.register(UserComments ,AdminUserComments)
