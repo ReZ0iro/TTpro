def394b7e45307d3d29e2cfc2846499375c84474
ReZ0iro <rezomoon@gmail.com>
2023-06-19 17:19:46 +0430

    Add Tables in posts.model:1.UserPosts thath show the post that user upload 2.UserComments : show the comments that user upload for the post !

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class UserPost(models.Model) : 
+
+    text    = models.TextField(blank=False)
+
+    the_user    = models.ForeignKey("Accounts.UserAccounts" , on_delete=models.CASCADE)
+
+
+class UserComments(models.Model) : 
+
+    comment_text    = models.TextField(blank=False) 
+    
+    user_comment    = models.ForeignKey("Accounts.UserAccounts", on_delete=models.CASCADE) 
+
+    user_post       = models.ForeignKey(UserPost , on_delete=models.CASCADE, blank=False, null=False) 
+
