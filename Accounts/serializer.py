a1de964bc0922e8b7f247fcf58078d9eaa37bfac
ReZ0iro <rezomoon@gmail.com>
2023-06-21 20:43:17 +0430

    Add CreateUserSerializer To complete adding user by admin !

--- Accounts/serializer.py
+++ Accounts/serializer.py
@@ -7,10 +7,11 @@ from django.contrib.auth import get_user_model
 class UserSerializer(serializers.ModelSerializer) : 
 
     class Meta : 
+
         model     = get_user_model()
         fields    = "__all__"
 
-class CreateAdminUserSerializer(serializers.ModelSerializer) : 
+class CreateAdminUserSerializer(serializers.ModelSerializer) :
     class Meta : 
         model   = get_user_model()
         fields  = ("first_name" , "last_name" , "username" , "email"  , "password")
@@ -19,11 +20,31 @@ class CreateAdminUserSerializer(serializers.ModelSerializer) :
 
     def create(self,validated_data) : 
             user = get_user_model().objects.create_admin(
+                 
                 validated_data["username"] ,
                 validated_data["email"] ,
                 validated_data["first_name"] , 
                 validated_data["last_name"] ,
-                # validated_data["phone_number"] ,
                 validated_data["password"] , 
+
             )
             return user
+
+class CreateUserSerializer(serializers.ModelSerializer) : 
+
+    class Meta : 
+        model   = get_user_model()
+        fields  = ("username", "email", "first_name", "last_name")
+
+        extra_kwargs = {"password" : {"write_only" : True}}
+    def create(self,validated_data) : 
+
+        user    = get_user_model().objects.create_user(     
+             validated_data["username"] , 
+             validated_data["email"]    ,
+             validated_data["first_name"] ,
+             validated_data["last_name"] ,
+        )
+
+        return user
+    
